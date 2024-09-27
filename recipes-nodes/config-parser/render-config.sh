a5e7e1724cb93fd594f63769a47f50b6860929ec
bakhtin <a@bakhtin.net>
2024-09-27 13:20:16 +0100

    Fix typo
    Signed-off-by: bakhtin <a@bakhtin.net>

--- recipes-nodes/config-parser/render-config.sh
+++ recipes-nodes/config-parser/render-config.sh
@@ -7,7 +7,7 @@ export LC_ALL=C
 INPUT_CONFIG=$1
 TEMPLATE_FILE=$2
 
-jq -c '.' "$INPUT_CONFIG" | # make sure the input is valid JSON
+jq -c '.' "$INPUT_CONFIG" | # make sure the input is a valid JSON
   sed -E 's/[^[:graph:] ]//g' | # remove non-printable characters
   sed -E 's/\\(a|b|c|e|f|n|r|t|v|x[0-9a-f]{2}|u[0-9a-f]{4}|U[0-9a-f]{8}|[0-7]{3})/\\\\\1/g' | # escape sequences that have special meaning in Go (https://go.dev/ref/spec#Rune_literals)
   mustache $TEMPLATE_FILE
