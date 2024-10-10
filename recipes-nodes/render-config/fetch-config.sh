a821322b57fb02697e2231dec1abc846c1957b22
bakhtin <a@bakhtin.net>
2024-10-10 16:35:14 +0100

    Update reth sync config to use mustache template
    Signed-off-by: bakhtin <a@bakhtin.net>

--- recipes-nodes/render-config/fetch-config.sh
+++ recipes-nodes/render-config/fetch-config.sh
@@ -14,13 +14,13 @@ set -e
 source /etc/init-config.conf
 
 INIT_CONFIG_FILE="/etc/init-config.json"
-TEMPLATED_CONFIG_FILES="/etc/td-agent-bit/td-agent-bit.conf /etc/process-exporter/process-exporter.yaml /etc/prometheus/prometheus.yml /etc/rbuilder.config"
+TEMPLATED_CONFIG_FILES="/etc/td-agent-bit/td-agent-bit.conf /etc/process-exporter/process-exporter.yaml /etc/prometheus/prometheus.yml /etc/rbuilder.config /etc/rclone.conf"
 
 case "$1" in
   start)
     echo "Fetching configuration..."
     echo "Fetching configuration..." > /var/volatile/system-api.fifo
-    
+
     (umask 0177 && touch "${INIT_CONFIG_FILE}")
     curl -fsSL --proxy http://localhost:7937 --retry 3 --retry-delay 60 --retry-connrefused \
       -H "Metadata: true" -o "${INIT_CONFIG_FILE}" "${INIT_CONFIG_URL}"
